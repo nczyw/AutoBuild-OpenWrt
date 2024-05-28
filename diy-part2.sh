9ac1d430b40a6a311f9610782fa9163bb2ee689d
WenJun <nczywq@qq.com>
2024-05-28 15:54:44 +0800

    sublime

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-
 # 移除要替换的包
 rm -rf feeds/packages/net/v2ray-geodata
-#rm -rf feeds/packages/net/mosdns
-#rm -rf feeds/luci/applications/luci-app-mosdns
+
+#替换
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
 # Git稀疏克隆，只克隆指定目录到本地
 function git_sparse_clone() {
@@ -24,44 +14,21 @@ function git_sparse_clone() {
   mv -f $@ ../package
   cd .. && rm -rf $repodir
 }
-
-# 添加额外插件
-#git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-argon-config
-#git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
-#git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-aliddns
-#git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-filebrowser filebrowser
-#git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-jellyfin luci-lib-taskd luci-lib-xterm taskd
-#git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-linkease linkease ffmpeg-remux
-#git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-
-#git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-passwall2
-
-# 加入OpenClash核心
-#chmod -R a+x $GITHUB_WORKSPACE/preset-clash-core.sh
-#$GITHUB_WORKSPACE/preset-clash-core.sh
-
 # 添加passwall2
 git clone https://github.com/xiaorouji/openwrt-passwall2.git package/luci-app-passwall2
+# 添加万能推送
+git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-pushbot
 
 echo "
 
 CONFIG_PACKAGE_luci-app-passwall2=y
 
-" >> .config
+CONFIG_PACKAGE_luci-app-pushbot=y
 
+CONFIG_PACKAGE_luci-app-ssr-plus=n
+" >> .config 
 
 # 修改默认IP
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+# 修改默认网关
 sed -i 's/255.255.255.0/255.255.252.0/g' package/base-files/files/bin/config_generate
-# 修改默认主题
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
-# x86 型号只显示 CPU 型号
-#sed -i 's/${g}.*/${a}${b}${c}${d}${e}${f}${hydrid}/g' package/lean/autocore/files/x86/autocore
-
-# 修改本地时间格式
-#sed -i 's/os.date()/os.date("%a %Y-%m-%d %H:%M:%S")/g' package/lean/autocore/files/*/index.htm
-
-# 取消默认的 autosamba 依赖的 luci-app-samba 到 slim 里
-find  ./target/linux/ -maxdepth 2 -type f  -name Makefile -exec sed -i 's#autosamba##' {} \;
